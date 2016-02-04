4959b393efa4895eaf1073e5edc61bbcfefb87ec
unknown <Sittiporn@PROGRAMMER126.silkspan.local>
2016-02-04 10:02:56 +0700

    first commit

---
+++ c-Shap_consoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace c_Shap_consoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var obj =   MODEL.Model.systemType.callWebUrl;
+           
+            switch (obj)
+           {
+               case MODEL.Model.systemType.callWebUrl:
+                   allclass.callWeb.RunApp(args);
+                   break ; 
+           }
+        }
+    }
+}
---
+++ c-Shap_consoleApp/allclass/callWeb.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace c_Shap_consoleApp.allclass
+{
+	public class callWeb
+	{
+        public  static void RunApp(string[] args)
+        { 
+            Process proc = new Process();
+            proc.StartInfo.FileName = @"C:\Program Files\Mozilla Firefox\firefox.exe";
+            if (args.Length > 0)
+            {
+                proc.StartInfo.Arguments = @"http://localhost:52988/Stat/Default.aspx?mail=y";
+            }
+            else
+                proc.StartInfo.Arguments = @"http://192.168.0.53/ClaimService/stat/default.aspx?mail=y";
+            proc.Start(); 
+        }
+	}
+}
