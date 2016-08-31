5327c90c05702cf0fcd5f19bf09df4024919c2ed
Sittiporn <sittiporn@silkspan.local>
2016-08-31 09:21:36 +0700

    update code

--- c-Shap_consoleApp/Program.cs
+++ c-Shap_consoleApp/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
+using System.IO;
 
 namespace c_Shap_consoleApp
 {
@@ -9,15 +11,54 @@ namespace c_Shap_consoleApp
     {
         static void Main(string[] args)
         {
-
-           var obj =   MODEL.Model.systemType.callWebUrl;
            
-            switch (obj)
-           {
-               case MODEL.Model.systemType.callWebUrl:
-                   allclass.callWeb.RunApp(args);
-                   break ; 
-           }
+            string deviceId = getItem();
+            Console.WriteLine(deviceId);
+            Console.ReadKey();
+
+            //for (int i = 0; i <= 5; i++)
+            //{
+            //    var obj = MODEL.Model.systemType.callWebUrl;
+
+            //    switch (obj)
+            //    {
+            //        case MODEL.Model.systemType.callWebUrl:
+            //            allclass.callWeb.RunApp(args);
+            //            break;
+            //    }
+            //}
+        }
+        public static string getItem()
+        {
+
+            WebRequest request = WebRequest.Create("https://graph.facebook.com/v2.7/act_104383949750732/campaigns?fields=insights.fields(social_spend%2Cspend%2Cfrequency%2Csocial_clicks%2Cclicks%2Creach%2Ccampaign_name%2Cadset_delivery)&access_token=EAAHrti6pKZCYBAPRnNw0dZB4g5N05c4stcJJwr6iQYPsEmXxpdp8v9KlXkZBLZABQvNFQdWcDpgKpXGxBIxnwgsV7sZAQrjZB1jCGsSXnkIHZCfl4mZCdDlsjTNZBZAA1NmBWVj7b43jOsJmDHQN8s5dqxozbZC5HIzc094YacQYNvIhQZDZD&__mref=message_bubble");
+
+                request.Method = "POST"; 
+                // Set the ContentType property of the WebRequest.
+                request.ContentType = "application/x-www-form-urlencoded";
+
+                // Get the request stream.
+                Stream dataStream = request.GetRequestStream();
+
+                // Close the Stream object.
+                dataStream.Close();
+
+                // Get the response.
+                WebResponse response = request.GetResponse();
+                dataStream = response.GetResponseStream();
+
+                // Open the stream using a StreamReader for easy access.
+                StreamReader reader = new StreamReader(dataStream);
+
+                // Read the content.
+                string accesstoken = reader.ReadToEnd();
+                Console.WriteLine("xxx");
+
+                // Clean up the streams.
+                reader.Close();
+                dataStream.Close();
+                response.Close();
+                return accesstoken;
         }
     }
 }
--- c-Shap_consoleApp/allclass/callWeb.cs
+++ c-Shap_consoleApp/allclass/callWeb.cs
@@ -10,15 +10,19 @@ namespace c_Shap_consoleApp.allclass
 	{
         public  static void RunApp(string[] args)
         { 
-            Process proc = new Process();
+                Process proc = new Process();
             proc.StartInfo.FileName = @"C:\Program Files\Mozilla Firefox\firefox.exe";
             if (args.Length > 0)
             {
-                proc.StartInfo.Arguments = @"http://localhost:52988/Stat/Default.aspx?mail=y";
+                proc.StartInfo.Arguments = @"http://192.168.0.2";
             }
             else
-                proc.StartInfo.Arguments = @"http://192.168.0.53/ClaimService/stat/default.aspx?mail=y";
-            proc.Start(); 
+                proc.StartInfo.Arguments = @"http://www.silkspan.com/banner/log_cbanner_counter.asp?fname=http://www.silkspan.com/silkspan_ssl/credit/detail_cc_citi_platinum_rewards.asp&typedealer=Toi";
+                //proc.StartInfo.Arguments = @"http://192.168.0.2/car_loan/detail_carloan.asp?typedealer=toi";
+               // proc.StartInfo.Arguments = @"http://www.silkspan.com";
+            
+            proc.Start();  
+            proc.Close(); 
         }
 	}
 }
