d51a3f16a67fdb7f4bda67f3a1edb4020a3733af
Sittiporn <sittiporn@silkspan.local>
2016-09-08 18:13:21 +0700

    update code facebook api

--- c-Shap_consoleApp/Program.cs
+++ c-Shap_consoleApp/Program.cs
@@ -20,57 +20,12 @@ namespace c_Shap_consoleApp
            //    case MODEL.Model.systemType.callWebUrl:
            //        allclass.callWeb.RunApp(args);
            //        break ; 
-           //}  
-           
-           // fbApi(); 
-            //allclass.cCallFacebook a = new allclass.cCallFacebook();
-            //a.getChatFacebook();
+           //}   
+            
+            allclass.cCallFacebook a = new allclass.cCallFacebook();
+            a.getChatFacebook();  
 
+        }   
 
-         
-
-        }
-          
-
-        public static void fbApi() {
-
-            const string applicationId = "540642676124662";
-            const string applicationSecret = "329c5070ac5d9ca074f8233437f77078";
-            const string pageId = "102661313114041";
-
-            var fb = new FacebookClient();//applicationId,
-            dynamic result = fb.Get("oauth/access_token", new
-            {
-                client_id = applicationId,
-                client_secret = applicationSecret,
-                grant_type = "client_credentials"
-            });
-            fb.AccessToken = result.access_token;
-
-            string g = "xxx";
-
-            //https://business.facebook.com/settings/system-users?business_id=104383949750732
-
-            //dynamic parametersa = new ExpandoObject();
-            //parametersa.fields = "id,name,last_name";
-
-            //dynamic result1 = fb.Get("390936304428827", parametersa);
-            //var id = result1.id;
-            //var name = result1.name;
-
-
-            var parameters = new Dictionary<string, object>();
-            parameters["fields"] = "insights.fields(social_spend,spend,frequency,social_clicks,clicks,reach,campaign_name,adset_delivery)";
-            parameters["access_token"] = "EAAHrti6pKZCYBAMDbwOT8BOn5fPKaRNO0nZBwkDg6QikqIaGrj902eAVHZCz0ZBW2oYdBhYW3urka2eiwZAudzfzH9gBNU5YNOWuWL0QjgGAZC7LZCqjtE9BGNoCPZARZBxNa906ILnsTII5nx1YbqCehOewO7EAgSvTr2vKR9q8CrAZDZD";
-            var _path ="http://graph.facebook.com/me?fields=id,name,picture";
-            //var rr = (IDictionary<string, object>)fb.Get(_path);
-            //var rr = (IDictionary<string, object>)fb.Get("graph.facebook.com/v2.7/act_104383949750732/campaigns?fields=insights.fields(social_spend,spend,frequency,social_clicks,clicks,reach,campaign_name,adset_delivery)&access_token=EAAHrti6pKZCYBAMDbwOT8BOn5fPKaRNO0nZBwkDg6QikqIaGrj902eAVHZCz0ZBW2oYdBhYW3urka2eiwZAudzfzH9gBNU5YNOWuWL0QjgGAZC7LZCqjtE9BGNoCPZARZBxNa906ILnsTII5nx1YbqCehOewO7EAgSvTr2vKR9q8CrAZDZD");
-           
-            // var result2 = (IDictionary<string, object>)fb.Get("4", parameters);
-            //var id = (string)result2["id"];
-            //var name = (string)result2["name"];
-
-
-        }
     }
 }
--- c-Shap_consoleApp/allclass/cCallFacebook.cs
+++ c-Shap_consoleApp/allclass/cCallFacebook.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Facebook;
+using System.Net;
+using System.IO;
 
 namespace c_Shap_consoleApp.allclass
 {
     public class cCallFacebook
     {
 
-        public static string AccessToken = "EAAWh0ZAZAHVgcBABynj6lyl2oIUOjpKZBQX2xK3YmnrOlvVqeqqDlbzLdMZCa7pWKIaHw0gYpEYlVrlRCyL39PGp10BmmGiVMhVHdA4L2aFIaw7XTEZChhotQIFITjFNDegZBSFZBGeHFBJmNoTGqTB31R0kcD9wy2rWUcZBRUEtNQZDZD";
+        //public static string AccessToken = "EAAWh0ZAZAHVgcBABynj6lyl2oIUOjpKZBQX2xK3YmnrOlvVqeqqDlbzLdMZCa7pWKIaHw0gYpEYlVrlRCyL39PGp10BmmGiVMhVHdA4L2aFIaw7XTEZChhotQIFITjFNDegZBSFZBGeHFBJmNoTGqTB31R0kcD9wy2rWUcZBRUEtNQZDZD";
+        //public static string AppId = "1585296425047559";
+        //public static string AppSecrete = "bc2c8d0d5a808d54dd3368b00f627689";
+
+        public static string AccessToken  = "EAAWh0ZAZAHVgcBAPP8ueUZAneYcC69m1VQyZAVax0vSUZCskbShChcXx8XaZAp3M3DopsZBq5QpyFSxOyE3c7TomAycMnDIZA5qJZCh4WonWwOKmx5opCeUAnaMC8zioiphmydFfrSdZC5ZAKU0KrYKJCEtplGJ8TgIhCZB9SYfwQRp3DgZDZD";
         public static string AppId = "1585296425047559";
         public static string AppSecrete = "bc2c8d0d5a808d54dd3368b00f627689";
-
 
         /***
          remark: 
@@ -21,44 +26,33 @@ namespace c_Shap_consoleApp.allclass
          
          * refer : http://blog.prabir.me/posts/facebook-csharp-sdk-making-requests
          */
-        public cCallFacebook() {
+        public cCallFacebook() {   
 
-            AccessToken = getToken();
-            getCode();
-        }
-        public string getToken() { 
-
-            var fb = new FacebookClient();//applicationId,
-            dynamic result = fb.Get("oauth/access_token", new
-            {
-                client_id = AppId,
-                client_secret = AppSecrete,
-                grant_type = "client_credentials"
-            });
-            fb.AccessToken = result.access_token;
-            return result.access_token;
-        }
-        public string getCode()
+        } 
+        public object excuteGraph(string _graphUrl)
         {
-
-            var fb = new FacebookClient();//applicationId,
-            dynamic result = fb.Get("oauth/authorize?client_id="+AppId+"&redirect_uri=http://www.silkspan.com/carinsur/test/testfb.aspx&scope=ads_management,manage_pages");
-            //dynamic result = fb.Get("oauth/authorize?client_id="+AppId+"&redirect_uri="http://www.silkspan.com/carinsur/test/testfb.aspx&scope=ads_management,manage_pages", new
-            //{
-            //    client_id = AppId,
-            //    redirect_uri = "http://www.silkspan.com/carinsur/test/testfb.aspx",
-            //    scope = "ads_management,manage_pages"
-            //}); 
-            return result;
+            try
+            {
+                var fb = new FacebookClient(AccessToken) { AppId = AppId, AppSecret = AppSecrete }; 
+                List<object> _lisObj = new List<object>();
+                return fb.Get(_graphUrl); 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.ReadKey();
+                return null;
+            }
         }
-        public   IEnumerable<string> getChatFacebook()
+
+
+        public   object getChatFacebook()
         {
             try
             {
                 var fb = new FacebookClient(AccessToken) { AppId = AppId, AppSecret = AppSecrete };
                 List<string> _lisObj = new List<string>();
-                dynamic results = fb.Get("/silkspan/?fields=conversations");
-                 
+                dynamic results = fb.Get("/silkspan/?fields=conversations"); 
                 foreach (var result in results.data)
                 { 
                     // _lisObj.Add(result.id);
@@ -72,5 +66,39 @@ namespace c_Shap_consoleApp.allclass
                 return null;
             }
         }
+       // public IEnumerable<string> getFeed()
+        public object getFeed()
+        {
+            try
+            {
+                var fb = new FacebookClient(AccessToken) { AppId = AppId, AppSecret = AppSecrete };
+                List<object> _lisObj = new List<object>();
+                List<object> _lisObj2 = new List<object>();
+                dynamic results = fb.Get("/55269232341_10154511844402342/comments");
+                var a = results[1].next;
+
+                dynamic results2 = fb.Get(a);
+                var b = results[1].next;
+                foreach (object result in results2.data)
+                {
+                    _lisObj2.Add(result);
+                }
+                var c = results2[1].next;
+
+
+                foreach (object result in results.data)
+                {
+                     _lisObj.Add(result);
+                }
+                return _lisObj;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.ReadKey();
+                return null;
+            }
+        } 
+
     }
 }
